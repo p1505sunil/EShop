fdef1720eb10f45be58edf8f32cfa8fb7ee57256
venkateshkalva <vkalva45@gmail.com>
2019-07-14 15:19:02 -0500

    created specification evaluvator in Infra

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Microsoft.eShopWeb.Infrastructure.Data
+namespace Microsoft.eShopWeb.Infrastructure.Data 
 {
     public class SpecificationEvaluator<T> where T : BaseEntity
     {
